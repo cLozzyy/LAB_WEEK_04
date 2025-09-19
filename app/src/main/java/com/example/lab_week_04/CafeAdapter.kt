ee58c0fccf89aa7df6ea6ee769408395188f8776
ncholasandre <andrenick1208@gmail.com>
2025-09-19 18:03:48 +0700

    Commit No. 4 – Assignment

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.lab_week_04
 
+import android.content.Context
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -9,13 +10,18 @@ val TABS_FIXED = listOf(
     R.string.janjijiwa_title,
     R.string.kopikenangan_title,
 )
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+class CafeAdapter(fragmentManager: FragmentManager,lifecycle: Lifecycle, private val context: Context) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
-    override fun createFragment(position: Int): Fragment
-    {
-        return CafeDetailFragment()
+    override fun createFragment(position: Int): Fragment {
+        val description = when (position) {
+            0 -> context.getString(R.string.starbucks_desc)
+            1 -> context.getString(R.string.janjijiwa_desc)
+            2 -> context.getString(R.string.kopikenangan_desc)
+            else -> "Deskripsi tidak ditemukan"
+        }
+        return CafeDetailFragment.newInstance(description)
     }
 }
